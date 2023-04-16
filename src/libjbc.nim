782cf3d4d5b1214bb444a0a455ae96610bac5855
ac2pic <ac2pic@gmail.com>
2023-04-15 23:29:17 -0500

    Add sandbox functions

--- src/libjbc.nim
+++ src/libjbc.nim
@@ -4,10 +4,17 @@ import macros
 import posix
 {.passl: "-ljbc".}
 
-proc sudo_call*(cb: proc {.closure.}) =
+proc sudo_call*(cb: proc {.closure.}) = 
   jbc_run_as_root(rawProc(cb), rawEnv(cb), CWD_KEEP)
 
-template sudo*(body:untyped) {.dirty.} =
-  sudo_call(proc() {.closure.} =
-    body)
+proc sudo_call_sandbox*(cb: proc {.closure.}) =
+  jbc_run_as_root_in_sandbox(rawProc(cb), rawEnv(cb))
 
+template sudo*(body:untyped) {.dirty.} =
+  sudo_call_sandbox(proc() {.closure.} =
+    body)
+proc sudo_mount*(system_path: string, local_path: string) : cint =
+ jbc_mount_in_sandbox(system_path.cstring, local_path.cstring)
+
+proc sudo_unmount*(local_path: string) : cint =
+  jbc_unmount_in_sandbox(local_path.string)
--- src/libjbc/private/utils.nim
+++ src/libjbc/private/utils.nim
@@ -2,6 +2,8 @@ const
   CWD_KEEP* = 0
   CWD_ROOT* = 1
   CWD_RESET* = 2
+proc jbc_run_as_root_in_sandbox*(fn, arg: pointer) {.cdecl, importc: "jbc_run_as_root_in_sandbox",
+                                        header: "libjbc.h".}
 
 proc jbc_run_as_root*(fn: pointer; arg: pointer;
                       cwd_mode: cint) {.cdecl, importc: "jbc_run_as_root",
@@ -10,3 +12,4 @@ proc jbc_mount_in_sandbox*(system_path: cstring; mnt_name: cstring): cint {.
     cdecl, importc: "jbc_mount_in_sandbox", header: "libjbc.h".}
 proc jbc_unmount_in_sandbox*(mnt_name: cstring): cint {.cdecl,
     importc: "jbc_unmount_in_sandbox", header: "libjbc.h".}
+
